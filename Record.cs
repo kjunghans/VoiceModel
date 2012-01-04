186aef73b0c85e7a474b7bbe76f84f5dc25d4af3
kjunghans_cp <SND\kjunghans_cp>
2012-01-04 18:56:51 +0000

    Make confirmation optional and ability to modify confirmation prompts.

--- Record.cs
+++ Record.cs
@@ -9,26 +9,57 @@ namespace VoiceModel
     {
         public List<Prompt> prompts { get; set; }
         public string recordingUrl { get; set; }
+        public bool confirm { get; set; }
+        public List<Prompt> confirmationPrompts { get; set; }
+        public int maxtime { get; set; }
+
+        public string maxtimeStr
+        {
+            get {return maxtime.ToString() + "s";}
+        }
         
         public Record()
         {
+            this.confirm = true;
+            this.confirmationPrompts = new List<Prompt>();
+            Prompt cprompt = new Prompt("I heard you say ");
+            cprompt.audios.Add(new TtsVariable("CallersMessage"));
+            cprompt.audios.Add(new Silence(1000));
+            cprompt.audios.Add(new TtsMessage("To save this message say yes. To discard it say no."));
+            this.confirmationPrompts.Add(cprompt);
             this.viewName = "Record";
             this.prompts = new List<Prompt>();
             this.recordingUrl = "SaveRecording";
+            this.maxtime = 60;
         }
 
         public Record(VxmlDocument doc)
            : base(doc)
         {
 
+            this.confirm = true;
+            this.confirmationPrompts = new List<Prompt>();
+            Prompt cprompt = new Prompt("I heard you say ");
+            cprompt.audios.Add(new TtsVariable("CallersMessage"));
+            cprompt.audios.Add(new Silence(1000));
+            cprompt.audios.Add(new TtsMessage("To save this message say yes. To discard it say no."));
+            this.confirmationPrompts.Add(cprompt);
             this.viewName = "Record";
             this.prompts = new List<Prompt>();
             this.recordingUrl = "SaveRecording";
+            this.maxtime = 60;
         }
 
         public Record(VxmlDocument doc, string id, string altText)
             : base(doc)
         {
+            this.confirm = true;
+            this.confirmationPrompts = new List<Prompt>();
+            Prompt cprompt = new Prompt("I heard you say ");
+            cprompt.audios.Add(new TtsVariable("CallersMessage"));
+            cprompt.audios.Add(new Silence(1000));
+            cprompt.audios.Add(new TtsMessage("To save this message say yes. To discard it say no."));
+            this.confirmationPrompts.Add(cprompt);
             this.viewName = "Record";
             this.id = id;
             this.prompts = new List<Prompt>();
@@ -36,10 +67,18 @@ namespace VoiceModel
             p.bargein = false;
             this.prompts.Add(p);
             this.recordingUrl = "SaveRecording";
+            this.maxtime = 60;
         }
 
         public Record(string id, string altText)
-         {
+        {
+            this.confirm = true;
+            this.confirmationPrompts = new List<Prompt>();
+            Prompt cprompt = new Prompt("I heard you say ");
+            cprompt.audios.Add(new TtsVariable("CallersMessage"));
+            cprompt.audios.Add(new Silence(1000));
+            cprompt.audios.Add(new TtsMessage("To save this message say yes. To discard it say no."));
+            this.confirmationPrompts.Add(cprompt);
             this.viewName = "Record";
             this.id = id;
             this.prompts = new List<Prompt>();
@@ -47,6 +86,7 @@ namespace VoiceModel
             p.bargein = false;
             this.prompts.Add(p);
             this.recordingUrl = "SaveRecording";
+            this.maxtime = 60;
         }
 
     }
