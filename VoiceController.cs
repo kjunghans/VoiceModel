9610dda37f1e3a52f6ff9bc1855e5f6f12b76faf
kjunghans_cp <SND\kjunghans_cp>
2012-01-17 21:41:09 +0000

    Changed VoiceController to enapsulate the caching and initialization process.

--- VoiceController.cs
+++ VoiceController.cs
@@ -4,16 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using VoiceModel.CallFlow;
 using System.IO;
 
 namespace VoiceModel
 {
-    public class VoiceController : Controller
+    public abstract class VoiceController : Controller
     {
         protected IVoiceModels voiceModels {get; set;}
         protected ICallFlow callFlow {get; set;}
         protected string recordingPath { get; set; }
+        const string vmCacheId = "record.vm";
+
+
+        private IVoiceModels GetVoiceModel()
+        {
+            VoiceModels views = (VoiceModels)HttpRuntime.Cache.Get(vmCacheId);
+            if (views == null)
+            {
+                views = BuildVoiceModels();
+                System.Web.HttpContext.Current.Cache.Insert(vmCacheId, views);
+            }
+            return views;
+
+        }
+
+        public abstract VoiceModels BuildVoiceModels();
+
+        const string cacheId = "record.cf";
+
+        private  ICallFlow GetCallFlow()
+        {
+            CallFlow.CallFlow flow = (CallFlow.CallFlow)HttpRuntime.Cache.Get(cacheId);
+            if (flow == null)
+            {
+                flow = BuildCallFlow();
+                System.Web.HttpContext.Current.Cache.Insert(cacheId, flow);
+            }
+            return flow;
+
+        }
+
+
+        public abstract CallFlow.CallFlow BuildCallFlow();
+
+        public  virtual string GetRecordingPath()
+        {
+            return "~/App_Data/recordings";
+        }
+
+        protected override void Initialize(RequestContext rc)
+        {
+            base.Initialize(rc);
+            voiceModels = GetVoiceModel();
+            callFlow = GetCallFlow();
+            recordingPath = GetRecordingPath();
+        }
+
 
         protected ActionResult VoiceView(string id, string vEvent, string json)
         {
