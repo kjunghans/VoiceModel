c14e8b3c69e04c2211bc12669a76a33b0f56bd89
kjunghans_cp <SND\kjunghans_cp>
2012-01-27 20:54:38 +0000

    More changes to handle reusable components

---
+++ Component.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace VoiceModel
+{
+    public class Component : VxmlDocument
+    {
+        private string jsonInput;
+
+        public Component(string id, string name, ComponentInput input)
+        {
+            this.viewName = "Component";
+            this.id = id;
+            this.controllerName = name;
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            this.jsonInput = serializer.Serialize(input);
+        }
+ 
+    }
+}
