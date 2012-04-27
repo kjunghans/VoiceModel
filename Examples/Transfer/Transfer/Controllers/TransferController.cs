4dcf57fa5e315a0c60864838f38b67abe48aa8ad
kjunghans_cp <SND\kjunghans_cp>
2012-04-27 14:17:27 +0000

    Updated Transfer example to use new state machine.

--- Examples/Transfer/Transfer/Controllers/TransferController.cs
+++ Examples/Transfer/Transfer/Controllers/TransferController.cs
@@ -10,30 +10,17 @@ namespace Transfer.Controllers
 {
     public class TransferController : VoiceController
     {
-        public override VoiceModels BuildVoiceModels()
-        {
-            VoiceModels views = new VoiceModels();
-
-            views.Add(new VoiceModel.Transfer("xfer", "sip:test@127.0.0.1", new Prompt("Please hold while you are transfered to the next available agent." )));
-            views.Add(new Exit("xferComplete", "The transfer has been completed."));
-            views.Add(new Exit("xferBusy", "That line is busy."));
-            views.Add(new Exit("xferNoAnswer", "Sorry, no one answered the phone."));
-
-            return views;
-
-        }
-
+ 
         public override CallFlow BuildCallFlow()
         {
             CallFlow flow = new CallFlow();
-
-            flow.AddStartState(new State("xfer", "xferComplete")
+            flow.AddState(ViewStateBuilder.Build("xfer", "xferComplete", 
+                new VoiceModel.Transfer("xfer", "sip:test@127.0.0.1", new Prompt("Please hold while you are transfered to the next available agent.")))
                 .AddTransition("busy","xferBusy",null)
-                .AddTransition("noanswer","xferNoAnswer",null));
-            flow.AddState(new State("xferBusy"));
-            flow.AddState(new State("xferNoAnswer"));
+                .AddTransition("noanswer","xferNoAnswer",null),true);
+            flow.AddState(ViewStateBuilder.Build("xferBusy",new Exit("xferBusy", "That line is busy.")));
+            flow.AddState(ViewStateBuilder.Build("xferNoAnswer",new Exit("xferNoAnswer", "Sorry, no one answered the phone.")));
             return flow;
-
         }
 
     }
