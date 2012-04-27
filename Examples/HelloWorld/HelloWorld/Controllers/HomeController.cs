8d7afe3ca84fbe65f47e386a462f433b4cdfcd9b
kjunghans_cp <SND\kjunghans_cp>
2012-04-27 13:56:51 +0000

    Updated Hello World example to work with new state machine.

--- Examples/HelloWorld/HelloWorld/Controllers/HomeController.cs
+++ Examples/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -10,24 +10,15 @@ namespace HelloWorld.Controllers
 {
     public class HomeController : VoiceController
     {
-        public override VoiceModels BuildVoiceModels()
-        {
-            VoiceModels views = new VoiceModels();
-
-            //Use the Exit object instead of the Say object
-            //because this just plays a prompt and exits.  The 
-            //Say object expects a transition to another state
-            //or object.
-            views.Add(new Exit("greeting", "Hello World"));
-
-            return views;
-
-        }
 
         public override CallFlow BuildCallFlow()
         {
             CallFlow flow = new CallFlow();
-            flow.AddStartState(new State("greeting"));
+            //Use the Exit object instead of the Say object
+            //because this just plays a prompt and exits.  The 
+            //Say object expects a transition to another state
+            //or object.
+            flow.AddState(ViewStateBuilder.Build("greeting", new Exit("greeting", "Hello World")), true);
             return flow;
 
         }
