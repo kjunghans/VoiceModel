ab15b5419ae9f94801106ad239a404562bd55dff
kjunghans_cp <kjunghans@users.noreply.github.com>
2012-05-18 20:01:59 +0000

    Initial updates to Grammar object for better speech rec support. Not tested.

---
+++ Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoiceModel
+{
+    public class Item
+    {
+        string uterance { get; set; }
+        string ruleref { get; set; }
+        string tag { get; set; }
+        OneOf oneof { get; set; }
+
+        public Item(string uterance)
+        {
+            this.uterance = uterance;
+        }
+    }
+}
---
+++ Rule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace VoiceModel
+{
+    [XmlElement("rule")]
+    public class Rule
+    {
+        enum Scope { isPrivate, isPublic };
+
+        [XmlAttribute("id")]
+        public string id { get; set; }
+        [XmlIgnore]
+        public Scope scope { get; set; }
+
+        [XmlAttribute("scope")]
+        public string strScope 
+        { 
+            get 
+            {
+                if (scope == null)
+                    return "private";
+                else if (scope == Scope.isPrivate)
+                    return "private";
+                else
+                    return "public";
+            } 
+        }
+        public List<OneOf> OneOfList { get; set; }
+
+        public Rule(string Id)
+        {
+            this.id = Id;
+            OneOfList = new List<OneOf>();
+        }
+    }
+}
