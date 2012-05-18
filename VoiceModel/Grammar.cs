a2b74eabeefd7913197ffdb50ae90e111cae9fa8
kjunghans_cp <SND\kjunghans_cp>
2012-05-18 20:01:59 +0000

    Initial updates to Grammar object for better speech rec support. Not tested.

--- VoiceModel/Grammar.cs
+++ VoiceModel/Grammar.cs
@@ -2,30 +2,75 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace VoiceModel
 {
+    [XmlElement("grammar")]
     public class Grammar
     {
+        
+        public enum Mode { voice, dtmf };
+        public enum GrammarType { xml, gsl };
+
+        [XmlAttribute("src")]
         public string source { get; set; }
-        public string type { get; set; }
-        public string inline { get; set; }
-        bool _isBuiltin = false;
-        public bool isBuiltin { get { return _isBuiltin; } }
-        public Grammar(string type)
-        {
-            _isBuiltin = true;
-            this.type = type;
+        [XmlAttribute("type")]
+        public string type 
+        { 
+            get 
+            {
+                if (gType == GrammarType.gsl)
+                    return "text/gsl";
+                else
+                    return "application/grammar-xml";
+            } 
         }
-        public Grammar(string type, ResourceLocation loc, string source)
+
+        [XmlIgnore]
+        public bool isBuiltin { get { return (!string.IsNullOrEmpty(_builtin)); } }
+        [XmlAttribute("xml:lang")]
+        public string language { get; set; }
+        [XmlAttribute("mode")]
+        public Mode mode { get; set; }
+        [XmlIgnore]
+        public GrammarType gType { get; set; }
+        [XmlAttribute("root")]
+        public string root { get; set; }
+        [XmlIgnore]
+        string _builtin;
+        [XmlIgnore]
+        public string builtin { get { return _builtin; } }
+
+        
+        public Rule rule { get; set; }
+
+        private void setDefaults()
         {
-            this.type = type;
+            this.gType = GrammarType.xml;
+            this.mode = Mode.voice;
+            this.language = "en-US";
+        }
+
+        public Grammar(string builtinGrammar)
+        {
+            _builtin = builtinGrammar;
+        }
+
+        public Grammar(ResourceLocation loc, string source)
+        {
+            setDefaults();
             this.source = loc.url + source;
         }
-        public Grammar(string type, string inline)
+
+        public Grammar(string Id, List<string> utternances)
         {
-            this.type = type;
-            this.inline = inline;
+            setDefaults();
+            root = Id;
+            rule = new Rule(Id);
+            rule.OneOfList.Add(new OneOf(utternances));
         }
+
+
     }
 }
---
+++ VoiceModel/OneOf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoiceModel
+{
+    public class OneOf
+    {
+        public List<Item> Items { get; set; }
+
+        public OneOf()
+        {
+            Items = new List<Item>();
+        }
+
+        public OneOf(List<string> uterances)
+        {
+            foreach (string uterance in uterances)
+                Items.Add(new Item(uterance));
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+    }
+}
