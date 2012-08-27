d778fa8b7ce0edf0e0b96b2ccc95180da55f4bb1
kjunghans_cp <kjunghans@users.noreply.github.com>
2012-08-27 20:28:55 +0000

    Added log4net support

---
+++ Logger/ILoggerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace VoiceModel.Logger
+{
+    public interface ILoggerService
+    {
+        void Debug(string message);
+        void Error(Exception ex);
+        void Error(string message);
+        void Fatal(Exception ex);
+        void Fatal(string message);
+        void Info(string message);
+        void Warn(string message);
+    }
+}
