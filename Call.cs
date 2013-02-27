b4ea36a5423d94182759af9a3a2d942850e834a6
kjunghans_cp <kjunghans@users.noreply.github.com>
2013-02-27 21:36:46 +0000

    Adding outbound call example. Not tested.

---
+++ Call.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VoiceModel
+{
+    public class Call: VoiceModel
+    {
+        public string to { get; set; }
+        public string from { get; set; }
+
+        public override VoiceModel BuildModel(string jsonArgs)
+        {
+            this.json = jsonArgs;
+            return this;
+        }
+
+    }
+}
