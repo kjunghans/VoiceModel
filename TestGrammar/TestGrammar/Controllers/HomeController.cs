81689112a747fbe98b310df381b56cf4627392a0
kjunghans_cp <SND\kjunghans_cp>
2013-07-30 20:17:39 +0000

    Solution for testing grammar output

---
+++ TestGrammar/TestGrammar/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VoiceModel;
+using VoiceModel.CallFlow;
+
+namespace TestGrammar.Controllers
+{
+    public class HomeController : VoiceController
+    {
+        public override CallFlow BuildCallFlow()
+        {
+            CallFlow flow = new CallFlow();
+            flow.AddState(ViewStateBuilder.Build("greeting", 
+                new Ask("greeting", new Prompt("Please respond to this grammar"), 
+                    new Grammar(new ResourceLocation(new Var(flow, "grammarLoc", "/grammars/")),"myGrammar.xml"))), true);
+            return flow;
+
+        }
+
+    }
+}
